390446c6e7234258b3952fc54e38ca0b40794042
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-25 03:22:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,4 +15,7 @@ class String
    def exclamation?
      self.end_with?("!")
    end 
+   def count_sentences
+     self.split(/[.?!]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+   end 
   end 
