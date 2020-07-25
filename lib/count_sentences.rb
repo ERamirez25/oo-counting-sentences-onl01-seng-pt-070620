7cfb0a788cca3b2f14f44675bf94e861368a7f26
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-25 03:12:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,24 +1,7 @@
 require 'pry'
 
 class String
-
-  def sentence?
-    "Hi, my name is Sophie.".end_with?(".")
-    
-  end
-
-  def question?
-  "What's your name?".end_with?("?")
-    
-    end
-
-  def exclamation?
-    "Hi, my name is Sophie!".end_with?("!")
-
-  end
-
-  def count_sentences
+ def sentence?
+   sef.end_with?(".")
+ end 
    
-
-  end
-end
