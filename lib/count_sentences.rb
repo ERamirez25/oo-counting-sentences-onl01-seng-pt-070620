02f9fc797c0faa6d25c2246887bc84172bd0f43b
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-25 03:05:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -8,11 +8,14 @@ class String
   end
 
   def question?
-    "What's your name?".end_with?("?")
+  "What's your name?".end_with?("?")
+  "Happy Halloween!".end_with?("?")
+    
 
   end
 
   def exclamation?
+    
 
   end
 
